64eb8431cbc3ac0438dc37c1115bad326b4f56fe
Brandon Hjelstrom <Brandon.Hjelstrom@gmail.com>
2024-04-05 08:55:27 -0700

    update examples

--- java-wpilib/swerve-megatag-odometry/src/main/java/frc/robot/Drivetrain.java
+++ java-wpilib/swerve-megatag-odometry/src/main/java/frc/robot/Drivetrain.java
@@ -112,6 +112,10 @@ public class Drivetrain {
           doRejectUpdate = true;
         }
       }
+      if(mt1.tagCount == 0)
+      {
+        doRejectUpdate = true;
+      }
 
       if(!doRejectUpdate)
       {
@@ -126,6 +130,10 @@ public class Drivetrain {
       LimelightHelpers.SetRobotOrientation("limelight", m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
       LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
       if(Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+      {
+        doRejectUpdate = true;
+      }
+      if(mt2.tagCount == 0)
       {
         doRejectUpdate = true;
       }
